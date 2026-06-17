9ac4ca71243e00af42828a4d20a700d91781013b
A5HxD <prasadayush05@gmail.com>
2026-06-17 20:58:47 +0530

    Updated indentation

--- src/train.py
+++ src/train.py
@@ -1,3 +1,7 @@
+import mlflow   # Library for experiment tracking, model packaging, evaluation, and deployment
+import mlflow.sklearn   # It tracks, saves, and loads scikit-learn machine learning models.
+
+
 import joblib       # Library for saving and loading machine learning models
 
 from sklearn.pipeline import Pipeline   # Library that chains multiple data processing steps and an estimator into a single, unified object.
@@ -24,83 +28,89 @@ from config import MODEL_PATH   # Import the model path from the config file
 
 def train():    # Create a function to initiate model training pipeline
 
-    df=load_data()  # Load Data
-
-    df=clean_data(df)   # Clean data
-
-    X,y=split_data(df)  # Split data into features and class
-
-    X_train, X_test, y_train, y_test = train_test_data(X,y) # Split data into testing and training data
-
-    preprocessor = build_preprocessor(  # Process the training data (normalize)
-        X_train
+    mlflow.set_experiment(
+        "Customer_Churn_analysis"
     )
 
-    # Create a pipeline
-    model = Pipeline(       # Pipeline() offers same preprocessing during training and prediction which prevents deployment bugs
+    with mlflow.start_run():
 
-        steps=[     # Define the sequence of steps to be executed
-            (
-                "preprocessor", preprocessor    # Preprocessing step: Apply scaling and encoding
-            ),
-            (
-                "classifier", LogisticRegression(max_iter=1000) # Specify the model for training: Logistic regression used for prediction
+        df=load_data()  # Load Data
+
+        df=clean_data(df)   # Clean data
+
+        X,y=split_data(df)  # Split data into features and class
+
+        X_train, X_test, y_train, y_test = train_test_data(X,y) # Split data into testing and training data
+
+        preprocessor = build_preprocessor(  # Process the training data (normalize)
+            X_train
+        )
+
+        # Create a pipeline
+        model = Pipeline(       # Pipeline() offers same preprocessing during training and prediction which prevents deployment bugs
+
+            steps=[     # Define the sequence of steps to be executed
+                (
+                    "preprocessor", preprocessor    # Preprocessing step: Apply scaling and encoding
+                ),
+                (
+                    "classifier", LogisticRegression(max_iter=1000) # Specify the model for training: Logistic regression used for prediction
+                )
+            ]
+        )
+
+        model.fit(      # Train the pipeline
+            X_train, y_train    # Feature variables and target variables used for training
+        )
+
+        predictions = model.predict(    # Generate class predictions on the test dataset
+            X_test      # Feature vraibles for test dataset
+        )
+
+        probabilities = model.predict_proba(    # Generate probability scores for each class predictiom
+            X_test
+        )[:,1]      # Extract only the class
+
+
+        # Display the evaluation metrics
+
+        print(
+            "Accuracy:", accuracy_score(    # Calculate the model accuracy
+                y_test, predictions     # Proportions of correct predictions
             )
-        ]
-    )
-
-    model.fit(      # Train the pipeline
-        X_train, y_train    # Feature variables and target variables used for training
-    )
-
-    predictions = model.predict(    # Generate class predictions on the test dataset
-        X_test      # Feature vraibles for test dataset
-    )
-
-    probabilities = model.predict_proba(    # Generate probability scores for each class predictiom
-        X_test
-    )[:,1]      # Extract only the class
-
-
-    # Display the evaluation metrics
-
-    print(
-        "Accuracy:", accuracy_score(    # Calculate the model accuracy
-            y_test, predictions     # Proportions of correct predictions
         )
-    )
 
-    print(
-        "Precision:", precision_score(  # Calculate the model precisiom
-            y_test, predictions     # percentage of True prediction that were actually true
+        print(
+            "Precision:", precision_score(  # Calculate the model precisiom
+                y_test, predictions     # percentage of True prediction that were actually true
+            )
         )
-    )
 
-    print(
-        "Recall:", recall_score(    # Calculate the model recall
-            y_test, predictions     # percentage of actual churn customers correctly identified
+        print(
+            "Recall:", recall_score(    # Calculate the model recall
+                y_test, predictions     # percentage of actual churn customers correctly identified
+            )
         )
-    )
 
-    print(
-        "F1:", f1_score(    # Calculate the F1 Score
-            y_test, predictions     # Calculates harmonic mean of precision and recall
+        print(
+            "F1:", f1_score(    # Calculate the F1 Score
+                y_test, predictions     # Calculates harmonic mean of precision and recall
+            )
         )
-    )
 
-    print(
-        "ROC-AUC:", roc_auc_score(      # Calculate the ROC-AUC score
-            y_test, probabilities   # Measures the model's ability to distinguish between churn and non-churn classes
+        print(
+            "ROC-AUC:", roc_auc_score(      # Calculate the ROC-AUC score
+                y_test, probabilities   # Measures the model's ability to distinguish between churn and non-churn classes
+            )
         )
-    )
 
-    joblib.dump(    # save the trained data
-        model,MODEL_PATH    # Define the model object and the model path
-    )
+        joblib.dump(    # save the trained data
+            model,MODEL_PATH    # Define the model object and the model path
+        )
 
-    print(
-        "\nModel saved..."  # Confirm saving
-    )
+        print(
+            "\nModel saved..."  # Confirm saving
+        )
 
 
 if __name__ == "__main__":  # Execute the training function only when this file is run directly
